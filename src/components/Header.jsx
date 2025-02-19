d3f0f022f1f150a472ab134b292facfbd75dd9bd
unknown <fsowah001@gmail.com>
2025-02-19 17:09:59 +0000

    added typewriting effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import "../css/Header.css"
 import headerImage from "../assets/dashboard-header.png"
 import CTAForm from "./CTAForm"
+import Typewriter from "typewriter-effect"
 
 function Header() {
-    return(
+    return (
         <div className="header-container">
             <div className="header-text">
-                <h1>Automate & Elevate Your School Management with AI</h1>
+                <h1>
+                    <Typewriter options={{
+                        strings: ["Automate & Elevate Your School Management with AI"],
+                        autoStart: true,
+                        loop: true,
+                    }} />
+                </h1>
                 <p>Our AI-driven School Management System empowers institutions with intelligent insights, streamlining operations, and optimizing academic performance.</p>
             </div>
             <div className="header-cta">
